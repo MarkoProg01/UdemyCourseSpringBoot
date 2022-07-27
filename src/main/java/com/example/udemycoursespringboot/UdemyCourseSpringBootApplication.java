1514ef5bc2cfa242aed3c645165564aff1b29744
MarkoProg01 <mmirkov2706@gmail.com>
2022-07-27 11:49:50 +0200

    empty

---
+++ src/main/java/com/example/udemycoursespringboot/UdemyCourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemycoursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCourseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyCourseSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/udemycoursespringboot/UdemyCourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemycoursespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCourseSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
